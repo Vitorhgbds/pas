f59219f1b7ce864de4293a5039217f817def6989
Julia <juliamakowski@hotmail.com>
2021-11-27 14:32:06 -0300

    added things

--- SvBkEndProt/src/main/java/com/bcopstein/adaptadores/Controller.java
+++ SvBkEndProt/src/main/java/com/bcopstein/adaptadores/Controller.java
@@ -1,4 +1,64 @@
 package com.bcopstein.adaptadores;
+/* TO DO
+    IMPORTS
+*/
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/requests")
 public class Controller {
+
+    @Resource
+    private ServicoVendas serviceVendas;
+    private ServicoDeEstoque service;
+
+    /*
+        Na rota de venda registrar codigo do produto, Quantidade, preco do produto,
+        Precisa diminuir esse valor do banco de dados local.
+    */
+
+    @GetMapping("/venda/produto") 
+    @CrossOrigin(origins = "*")
+    public void venda(@RequestParam int codigo, @RequestParam int qtd){
+        if(serviceVendas.venda(codigo, qtd))
+    }
+    
+    /*
+    public ResponseEntity<> estatisticas(@RequestParam final int distancia){
+        return new ResponseEntity<>(service.findById((long) distancia), OK);
+    }
+    */
+
+    /*
+        Produtos vendidos: deve retornar à relação de produtos disponíveis 
+        para venda. Comunicação síncrona
+    */
+    @GetMapping("/venda") 
+    @CrossOrigin(origins = "*")
+
+    /*
+        Chegada de itens no estoque: usada para informar a chegada de novos 
+        itens no estoque. Comunicação síncrona.
+    */
+
+    @GetMapping("/estoque/adicionar") 
+    @CrossOrigin(origins = "*")
+
+    /*
+        Baixa no estoque: deve atualizar a quantidade de um produto 
+        no estoque subtraindo a quantidade vendida. Deve sinalizar caso 
+        a operação não seja possível. Comunicação síncrona.
+        codigo do produto + quantidade
+    */
+
+    @GetMapping("/estoque/remover") 
+    @CrossOrigin(origins = "*")
+
+    /*
+        Desfazer baixa no estoque: deve somar novamente a quantidade vendida.
+        Deve ser usada para operações de “rollback”
+    */
+
+    @GetMapping("/estoque/rollback") 
+    @CrossOrigin(origins = "*")
 }
--- SvBkEndProt/src/main/java/com/bcopstein/negocio/servicos/ServicoVendas.java
+++ SvBkEndProt/src/main/java/com/bcopstein/negocio/servicos/ServicoVendas.java
@@ -1,4 +1,46 @@
 package com.bcopstein.negocio.servicos;
 
 public class ServicoVendas {
+
+    private final EstoqueRepository estoqueRepository;
+    private final ServicoDe
+
+    /*
+        Fazer metodos para ser usados
+        
+        - Armazenar uma venda no banco de dados
+        - Calcular, a partir do preco do produto e da quantidade, o subtotal, os impostos e o total a pagar
+        - Diminuir o produto do banco de dados do estoque
+  
+        - •	Implementar um mecanismo de “rollback” para as vendas. Se no 
+        momento de efetivar uma venda alguma das operações de “baixa no 
+        estoque” falhar, toda a venda deve ser cancelada e mensagens para
+        “repor” o que já foi retirado do estoque devem ser enviadas 
+        (20% da nota)
+    */
+
+    /*  Lista de vendas: responde com a relação total das vendas efetuadas
+        até agora. Deve ser usada comunicação síncrona através de um 
+        “controller”.
+    */
+    public 
+
+
+    // armazena venda no banco de dados, se for um sucesso ok, se não rollback
+    public boolean venda(int codigo, int qtdade){
+
+    }
+
+    // para verificar se possui produto no estoque
+    public boolean verifyEstoque(int codigo, int qtdade){
+        
+    }
+
+    public void rollback(){
+
+    }
+
+
+
+
 }
